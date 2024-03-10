9c4877a1eb9ac3aa1a8003ff62fe0106545e8113
imnotsatosh <imnotsatosh@proton.me>
2024-03-10 09:37:42 +0800

    modify GetBlockSubsidy

--- src/validation.cpp
+++ src/validation.cpp
@@ -1637,14 +1637,15 @@ PackageMempoolAcceptResult ProcessNewPackage(Chainstate& active_chainstate, CTxM
 
 CAmount GetBlockSubsidy(int nHeight, const Consensus::Params& consensusParams)
 {
-    int halvings = nHeight / consensusParams.nSubsidyHalvingInterval;
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return 0;
+    int drops = nHeight / consensusParams.nSubsidyHalvingInterval;
 
     CAmount nSubsidy = INITIAL_REWARD;
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
+    // Subsidy drop 1/DROP_DIVISOR every 105000 blocks which will occur approximately every year.
+    while(drops > 0)
+    {
+        nSubsidy -= nSubsidy/DROP_DIVISOR;
+        --drops;
+    }
     return nSubsidy;
 }
 
